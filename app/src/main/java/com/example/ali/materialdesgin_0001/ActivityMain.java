5586a8c267991c718399ab95164b274a4ee97fc6
Ali <aljae0719@gmail.com>
2018-11-03 23:23:21 +0330

    i changed 01101

---
+++ app/src/main/java/com/example/ali/materialdesgin_0001/ActivityMain.java
@@ -0,0 +1,15 @@
+package com.example.ali.materialdesgin_0001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
